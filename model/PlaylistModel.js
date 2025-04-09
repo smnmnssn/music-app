3a0111806b35d7444f7735cad830a20764e1b697
Simon <simonmansson@live.se>
2025-04-09 15:47:55 +0200

    added classes and methods for playlists

--- model/PlaylistModel.js
+++ model/PlaylistModel.js
@@ -1,12 +1,57 @@
+import Song from './Song.js'; // eller rätt sökväg beroende på din struktur
+
+
 class Playlist {
-  constructor(name, genre, artist) {
+  constructor(name, genre) {
     this.name = name;
     this.genre = genre;
-    this.artist = artist;
     this.songs = [];
   }
 
   addSong(song) {
     this.songs.push(song);
   }
+
+  removeSong(title) {
+    const index = this.songs.findIndex((s) => s.title === title);
+  
+    if (index !== -1) {
+      this.songs.splice(index, 1);
+    }
+  }
+  
+} 
+
+class PlaylistManager {
+  constructor() {
+    this.playlists = [];
+  }
+
+  createPlaylist(playlist) {
+    this.playlists.push(playlist);
+  }
+
+  deletePlaylist(name) {
+    const index = this.playlists.findIndex((p) => p.name === name);
+
+    if (index !== -1) {
+      this.playlists.splice(index, 1);
+    } 
+  }
+
+  getPlaylists() {
+    return this.playlists;
+
+  }
 }
+
+// === Skapar ett exempel för att testa ===
+const playlist = new Playlist("Min Spellista", "Rock");
+const song = new Song("Queen", "Bohemian Rhapsody", "Rock");
+
+playlist.addSong(song); // Lägger till låten i spellistan
+
+const manager = new PlaylistManager();
+manager.createPlaylist(playlist); // Lägger till spellistan i hanteraren
+
+console.log(manager.getPlaylists()); // Skriver ut spellistorna i konsolen
---
+++ model/SongModel.js
@@ -0,0 +1,9 @@
+class Song {
+  constructor(artist, title, genre) {
+    this.artist = artist;
+    this.title = title;
+    this.genre = genre;
+  }
+}
+
+export default Song;
